56653edd9cefebcae8caba7a31903f70d78f1cb8
alicema1202 <64507532+alicema1202@users.noreply.github.com>
2026-08-01 22:42:03 -0700

    add series

--- updateCatalog.js
+++ updateCatalog.js
@@ -3,6 +3,9 @@ const fs = require("fs");
 const API_KEY = process.env.TMDB_KEY;
 
 
+/**
+ * Check if a movie has a US digital release
+ */
 async function hasDigitalRelease(movieId) {
 
     const response = await fetch(
@@ -22,7 +25,7 @@ async function hasDigitalRelease(movieId) {
     }
 
 
-    // TMDB release type 4 = Digital
+    // TMDB type 4 = Digital
     return usRelease.release_dates.some(
         release => release.type === 4
     );
@@ -30,15 +33,18 @@ async function hasDigitalRelease(movieId) {
 
 
 
-async function getPopularMovies() {
+/**
+ * Get top 25 digital movies
+ */
+async function getTopMovies() {
 
-    let movies = [];
+    const movies = [];
     let page = 1;
 
 
     while (movies.length < 25) {
 
-        console.log(`Fetching TMDB page ${page}`);
+        console.log(`Fetching movie page ${page}`);
 
 
         const response = await fetch(
@@ -49,28 +55,17 @@ async function getPopularMovies() {
         const data = await response.json();
 
 
-        if (!data.results) {
-            throw new Error(
-                "TMDB did not return results"
-            );
-        }
-
-
         for (const movie of data.results) {
-
 
             if (movies.length >= 25) {
                 break;
             }
 
 
-            const digital = await hasDigitalRelease(movie.id);
-
-
-            if (digital) {
+            if (await hasDigitalRelease(movie.id)) {
 
                 console.log(
-                    "Added:",
+                    "Movie added:",
                     movie.title
                 );
 
@@ -79,34 +74,22 @@ async function getPopularMovies() {
                     id: `tmdb:${movie.id}`,
                     type: "movie",
                     name: movie.title,
-                    poster:
-                        movie.poster_path
+                    poster: movie.poster_path
                         ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
                         : null,
-                    background:
-                        movie.backdrop_path
+                    background: movie.backdrop_path
                         ? `https://image.tmdb.org/t/p/w1280${movie.backdrop_path}`
                         : null
                 });
 
-            } else {
-
-                console.log(
-                    "Skipped:",
-                    movie.title
-                );
-
             }
+
         }
 
 
         page++;
 
-
-        // Safety limit
-        if (page > 10) {
-            break;
-        }
+        if (page > 10) break;
     }
 
 
@@ -115,27 +98,86 @@ async function getPopularMovies() {
 
 
 
-async function updateCatalog() {
+/**
+ * Get top 25 TV series
+ */
+async function getTopSeries() {
 
-    if (!API_KEY) {
-        throw new Error(
-            "Missing TMDB_KEY"
+    const series = [];
+    let page = 1;
+
+
+    while (series.length < 25) {
+
+        console.log(`Fetching series page ${page}`);
+
+
+        const response = await fetch(
+            `https://api.themoviedb.org/3/discover/tv?api_key=${API_KEY}&sort_by=popularity.desc&vote_count.gte=100&page=${page}`
         );
+
+
+        const data = await response.json();
+
+
+        if (!data.results) {
+            throw new Error("TMDB TV returned no results");
+        }
+
+
+        for (const show of data.results) {
+
+            if (series.length >= 25) {
+                break;
+            }
+
+
+            console.log(
+                "Series added:",
+                show.name
+            );
+
+
+            series.push({
+                id: `tmdb:${show.id}`,
+                type: "series",
+                name: show.name,
+                poster: show.poster_path
+                    ? `https://image.tmdb.org/t/p/w500${show.poster_path}`
+                    : null,
+                background: show.backdrop_path
+                    ? `https://image.tmdb.org/t/p/w1280${show.backdrop_path}`
+                    : null
+            });
+
+        }
+
+
+        page++;
+
+        if (page > 10) break;
     }
 
 
-    const movies = await getPopularMovies();
+    return series;
+}
 
+
+
+/**
+ * Write JSON file
+ */
+function saveCatalog(path, name, metas) {
 
     const catalog = {
-        name: "TMDB Top 25 Digital Movies",
+        name,
         updated: new Date().toISOString(),
-        metas: movies
+        metas
     };
 
 
     fs.writeFileSync(
-        "catalog/movie/movieCatalog.json",
+        path,
         JSON.stringify(
             catalog,
             null,
@@ -145,8 +187,42 @@ async function updateCatalog() {
 
 
     console.log(
-        `Catalog updated: ${movies.length} movies`
+        `Saved ${path}`
     );
+}
+
+
+
+/**
+ * Main
+ */
+async function updateCatalog() {
+
+    if (!API_KEY) {
+        throw new Error("Missing TMDB_KEY");
+    }
+
+
+    const movies = await getTopMovies();
+
+    const series = await getTopSeries();
+
+
+    saveCatalog(
+        "catalog/movie/movieCatalog.json",
+        "TMDB Top 25 Movies",
+        movies
+    );
+
+
+    saveCatalog(
+        "catalog/series/seriesCatalog.json",
+        "TMDB Top 25 Series",
+        series
+    );
+
+
+    console.log("All catalogs updated!");
 }
 
 
